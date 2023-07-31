40a33a75901bdaf61c173eaf6ee914441a1d2152
dinhtronghao <dinhtronghao12102002@gmail.com>
2023-07-31 07:41:43 +0700

    update_lab7

--- bai2/bai2Client/app/src/main/java/com/example/bai2client/MainActivity.java
+++ bai2/bai2Client/app/src/main/java/com/example/bai2client/MainActivity.java
@@ -3,10 +3,19 @@ package com.example.bai2client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView textViewResult;
+    private LinearLayout LinearLayout1;
+    private EditText addressEditText;
+    private EditText portEditText;
+    private Button connectButton;
+    private Button clearButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +23,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textViewResult = findViewById(R.id.responseTextView);
 
-        // Set the IP and port of the server
-        String serverIP = "10.2.0.15";
-        int serverPort = 8080;
 
-        // Create an object of the Client class
-        Client client = new Client(serverIP, serverPort, textViewResult);
+        LinearLayout1 = (LinearLayout) findViewById(R.id.LinearLayout1);
+        addressEditText = (EditText) findViewById(R.id.addressEditText);
+        portEditText = (EditText) findViewById(R.id.portEditText);
+        connectButton = (Button) findViewById(R.id.connectButton);
+        clearButton = (Button) findViewById(R.id.clearButton);
 
-        // Execute the AsyncTask to connect to the server
-        client.execute();
+        connectButton.setOnClickListener(this);
+        clearButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.connectButton){
+            String serverIP = addressEditText.getText().toString().trim();
+            int serverPort = Integer.parseInt(portEditText.getText().toString().trim());
+            Client client = new Client(serverIP, serverPort, textViewResult);
+            // Execute the AsyncTask to connect to the server
+            client.execute();
+
+        } else if (view.getId() == R.id.clearButton) {
+            addressEditText.setText("");
+            portEditText.setText("");
+        }
     }
 }
